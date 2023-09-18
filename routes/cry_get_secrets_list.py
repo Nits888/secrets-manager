ce0eed97e64a20e4d7209e2922f84b454b36e09c
Nitin Chandel <Nits888@GMAIL.COM>
2023-09-18 09:36:34 +0530

    Final Version

--- routes/cry_get_secrets_list.py
+++ routes/cry_get_secrets_list.py
@@ -1,28 +1,49 @@
 import logging
+from http import HTTPStatus
+
+from flask import g
 from flask_restx import Namespace, Resource
-from globals import LOG_LEVEL
+from globals import LOG_LEVEL, auth_parser
 from modules import cry_secrets_management
+from modules.cry_auth import auth
 
 logging.basicConfig(level=LOG_LEVEL)
 
 ns = Namespace('get_secrets_list', description='Get Secrets List Route Namespace')
 
 
-@ns.route('/get_secrets_list/<string:bucket>')
+@ns.route('/get_secrets_list/<string:app_name>/<string:bucket>')
 class GetSecretList(Resource):
     """
     Resource to fetch the list of secrets for a given bucket.
     """
-    @staticmethod
-    def get(bucket):
+
+    @auth.login_required
+    @ns.expect(auth_parser, params={'app_name': 'Name of the Application',
+                                    'bucket': 'Name of the bucket',
+                                    'secret_name': 'Name of the secret'}, validate=True)
+    @ns.doc(security='apikey')
+    @ns.response(HTTPStatus.OK, 'Secret successfully retrieved.')
+    @ns.response(HTTPStatus.NOT_FOUND, 'Bucket or secret not found.')
+    @ns.response(HTTPStatus.UNAUTHORIZED, 'Unauthorized access.')
+    @ns.response(HTTPStatus.INTERNAL_SERVER_ERROR, 'Internal server error encountered.')
+    def get(self, bucket, app_name):
         """
         GET method to retrieve the list of secrets for the specified bucket.
 
         :param bucket: Name of the bucket for which to retrieve the secrets list.
+        :param app_name: Application Name
         :return: A JSON response containing a list of secrets or an error message.
         """
+        if not hasattr(g, 'bucket_name'):
+            logging.error("bucket_name not found in global context. Token might not be set properly.")
+            return {'message': 'Unauthorized access.'}, HTTPStatus.UNAUTHORIZED
+
+        if g.bucket_name != bucket:
+            return {'message': 'Unauthorized access to this bucket.'}, HTTPStatus.UNAUTHORIZED
+
         try:
-            secrets_list = cry_secrets_management.get_secrets(bucket)
+            secrets_list = cry_secrets_management.get_secrets(bucket, app_name)
             logging.info(f"Successfully fetched secrets list for bucket '{bucket}'.")
             return {'secrets': secrets_list}, 200
         except Exception as e:
---
+++ templates/static/main.js
@@ -0,0 +1,83 @@
+// Script to get current year for footer
+let currentYear = new Date().getFullYear();
+document.querySelector('.footer').innerHTML = `&copy; ${currentYear} Your Company Name. <a href="your-link-here">Link to Page</a>`;
+
+        $('#createBucketBtn').on('click', function() {
+            let appName = $('#appNameInput').val().trim();
+            let bucketName = $('#bucketNameInput').val().trim();
+            if (!appName) {
+                alert("Please enter an app name!");
+                return;
+            }
+            if (!bucketName) {
+                alert("Please enter a bucket name!");
+                return;
+            }
+            $.ajax({
+                url: "/create_bucket/",
+                type: "POST",
+                data: JSON.stringify({ app_name: appName, bucket: bucketName }),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function(data) {
+                    if (data.message) {
+                        alert("Bucket '" + bucketName + "' under App '" + appName + "' created successfully! An email has been sent to the owner if configured.");
+                        location.reload();
+                    } else {
+                        alert("Error creating bucket: " + data.error);
+                    }
+                },
+                error: function(xhr, status, error) {
+                    alert("An error occurred: " + xhr.responseText);
+                }
+            });
+        });
+
+    // ToggleTheme Functions
+    $(document).ready(function(){
+    // Check local storage for theme
+    if (localStorage.getItem('theme') === 'dark') {
+        $('body').attr('data-theme', 'dark');
+        $('.sun-icon').hide();
+        $('.moon-icon').show();
+    }
+
+    // Toggle theme on SVG icon click
+    $('#themeToggleIcon').on('click', function() {
+        if ($('body').attr('data-theme') === 'dark') {
+            $('body').removeAttr('data-theme');
+            localStorage.setItem('theme', 'light');
+            $('.sun-icon').show();
+            $('.moon-icon').hide();
+        } else {
+            $('body').attr('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+            $('.sun-icon').hide();
+            $('.moon-icon').show();
+        }
+    });
+});
+
+    // Search Functions
+
+$(document).ready(function() {
+    $("#searchInput").on("keyup", function() {
+        const value = $(this).val().toLowerCase();
+
+        // If the search bar is empty, collapse all
+        if (!value) {
+            $(".collapse").collapse('hide');
+            return;
+        }
+
+        $("table tbody tr").each(function() {
+            const row = $(this);
+            if (row.text().toLowerCase().indexOf(value) > -1) {
+                row.show();
+                row.closest('.collapse').collapse('show');  // Expand the parent collapsible
+            } else {
+                row.hide();
+            }
+        });
+    });
+});
